bb3e491e95b4d7256258c6ef6748448fd5d774fe
Barsoum <barsoumadly@gmail.com>
2023-08-07 23:26:56 +0300

    show and close modal

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// Selecting containers
+const modalEl = document.querySelector('.modal');
+const overlayEl = document.querySelector('.overlay');
+
+// Selecting buttons
+const btnsShowModal = document.querySelectorAll('.btn--show-modal');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+
+// Closing Modal
+const closeModal = function () {
+  modalEl.classList.add('hidden');
+  overlayEl.classList.add('hidden');
+};
+
+// Showing Modal
+btnsShowModal.forEach(btn =>
+  btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    modalEl.classList.remove('hidden');
+    overlayEl.classList.remove('hidden');
+  })
+);
+
+// Close Modal by using btn or overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlayEl.addEventListener('click', closeModal);
+
+// Close Modal using Esc button
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
